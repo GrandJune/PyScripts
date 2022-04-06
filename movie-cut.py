0ff8d670d92fa7f2fa3b69ad403f31216a1ff841
[Junee] <[978260577@qq.com]>
2022-04-06 15:04:30 +0800

    video cutting script

--- movie-cut.py
+++ movie-cut.py
@@ -6,15 +6,15 @@
 # Observing PEP 8 coding style
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 start_hour = 0
-start_min = 20
-start_second = 5
+start_min = 28
+start_second = 00
 start_time = start_hour * 3600 + start_min * 60 + start_second  # 设置开始时间(单位秒)
 
 end_hour = 0
-end_min = 39
-end_second = 40
+end_min = 49
+end_second = 00
 end_time = end_hour * 3600 + end_min * 60 + end_second
 # ffmpeg_extract_subclip("full.mp4", start_seconds, end_seconds, targetname="cut.mp4")
-inflie_path = r"C:\Users\Junee\Desktop\67-cut.mp4"
-outfile_path = "7-cut.mp4"
+inflie_path = r"C:\Users\Junee\Downloads\GMT20220405-104514_Recording_3440x1440.mp4"
+outfile_path = "Not Economists-Treatsure.mp4"
 ffmpeg_extract_subclip(inflie_path, start_time, end_time, targetname=outfile_path)
