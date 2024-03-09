5de8839b5a9ff12fce3f9347f6493426cefd232a
毕夏 <978260577@qq.com>
2024-03-09 21:36:49 +0800

    Reference Integration

---
+++ Reference/TOTO.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time     : 3/9/2024 20:15
+# @Author   : Junyi
+# @FileName: TOTO.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+import random
+
+# Generate a list of 6 unique random numbers between 1 and 49
+random_numbers = random.sample(range(1, 50), 6)
+
+print(random_numbers)
+# [10, 17, 5, 22, 43, 41]
+# [40, 15, 32, 9, 21, 18]
